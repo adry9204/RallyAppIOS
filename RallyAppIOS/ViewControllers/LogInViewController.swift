be17f1780fe20b9b66873ecb5bcfb78995bc8347
adry9204 <adriana920420@gmail.com>
2023-03-21 18:00:20 -0400

    Log In Scene UI completed. LogIn View Controller added to the controllers folder

---
+++ RallyAppIOS/ViewControllers/LogInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LogInViewController.swift
+//  RallyAppIOS
+//
+//  Created by Adriana Diaz Torres on 3/21/23.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        containerView.layer.cornerRadius = 15
+ 
+    }
+
+
+}
